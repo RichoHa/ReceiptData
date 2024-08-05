ccfdb7842fb37cba9a95d8987d46133af5e377fb
Richo Ha <Richard.dqg@gmail.com>
2024-08-05 16:33:39 +0800

    Added text and styles

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,29 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
+import { StyleSheet, View, Text } from 'react-native';
+import Colors from '../../constants/Colors';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <text style={styles.title}>My Desk Space</text>
+      <text style={styles.text}>My Computer</text>
+      <text style={styles.text}>My Keyboard</text>
+      <text style={styles.text}>My Mouse</text>
+      <text style={styles.text}>My Desk</text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: '700',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  text: {
+    fontWeight:'600',
+    fontSize:12,
+    marginVertical:10,
+    color: Colors.light.tint,
   },
 });
