b0bc98964fb0bed47188adde8789d38eeeffc4e4
Richo Ha <Richard.dqg@gmail.com>
2024-08-05 17:04:39 +0800

    Added data Object

---
+++ src/data/asset.ts
@@ -0,0 +1,57 @@
+const asset = [
+    {
+        id:1,
+        name:'desk',
+        price:200,
+        purchasedDate:'12/03/23',
+        purchasedFrom:'Ikea',
+        Area:'My Desk Space',
+        ReceiptImage:'URL Here',
+    },
+    {
+        id:2,
+        name:'keyboard',
+        price:100,
+        purchasedDate:'10/02/23',
+        purchasedFrom:'JB Hi FI',
+        Area:'My Desk Space',
+        ReceiptImage:'URL Here',
+    },
+    {
+        id:3,
+        name:'Computer',
+        price:5000,
+        purchasedDate:'02/07/21',
+        purchasedFrom:'Razer',
+        Area:'My Desk Space',
+        ReceiptImage:'URL Here',
+    },
+    {
+        id:4,
+        name:'Bike',
+        price:6000,
+        purchasedDate:'04/08/24',
+        purchasedFrom:'Mitsubishi',
+        Area:'My Garage',
+        ReceiptImage:'URL Here',
+    },
+    {
+        id:5,
+        name:'Helmet',
+        price:2000,
+        purchasedDate:'01/08/24',
+        purchasedFrom:'Shoei',
+        Area:'My Garage',
+        ReceiptImage:'URL Here',
+    },
+    {
+        id:6,
+        name:'Coffee Machine',
+        price:5000,
+        purchasedDate:'02/09/22',
+        purchasedFrom:'The Good Guys',
+        Area:'Kitchen',
+        ReceiptImage:'URL Here',
+    }
+
+]
