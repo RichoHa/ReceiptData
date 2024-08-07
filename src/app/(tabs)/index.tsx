e08ee8979ba4e6b4fdc15f2d712fc508de793e64
Richo Ha <Richard.dqg@gmail.com>
2024-08-07 20:35:13 +0800

    added FlatList into the application to show unique areas

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,38 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList} from 'react-native';
 import Colors from '../../constants/Colors';
 import asset from '../../../src/data/asset';
 
 //Get 
-const myAsset = asset;
+const ListArea = asset.map((e)=>{e.Area});
+//
+//
+//
+const uniqueAreas = [...new Set(asset.map((item) => item.Area))]
+
+
+//FlatList Breakdown
+//1. Data: This is the array of items to be displayed in the list.
+//2. RenderItem: This is a function that returns a component to render for each item in the data array.
+//3. KeyExtractor: This is a function that extracts a unique key for each item in the data array.
+
+// Other way of doing it is:
+// {uniqueAreas.map((area) => (
+//   <Text key={area} style={styles.title}>{area}</Text>
+// ))}
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      {asset.map((e)=>{
-        return <Text style={styles.title}>{e.Area}</Text>
-      })}
+
+      <FlatList
+      data={uniqueAreas}
+
+      renderItem={({item})=>(
+        <Text style={styles.title}>{item}</Text>
+      )}
+      keyExtractor={(item)=>item}
+      />
+        
     </View>
   );
 }
